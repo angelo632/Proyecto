e9cda5df9a29690c8e2ca89eb0f1278d3665f528
Angelo Tomala <136107210+angelo632@users.noreply.github.com>
2023-06-24 16:42:45 -0500

    Add files via upload

---
+++ ProyectoSoft/Auto.cs
@@ -0,0 +1,10 @@
+﻿namespace CompaniaAutos
+{
+    class Auto
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Stock { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
