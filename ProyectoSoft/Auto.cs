91099b2d9163920f11e3d516000299e0cb9c9346
Angelo Tomala <136107210+angelo632@users.noreply.github.com>
2023-07-19 12:26:48 -0500

    Add files via upload

--- ProyectoSoft/Auto.cs
+++ ProyectoSoft/Auto.cs
@@ -4,6 +4,7 @@
     {
         public string Marca { get; set; }
         public string Modelo { get; set; }
+        public string Placa { get; set; }
         public int Stock { get; set; }
         public decimal Precio { get; set; }
     }
--- ProyectoSoft/Program.cs
+++ ProyectoSoft/Program.cs
@@ -66,7 +66,7 @@ namespace CompaniaAutos
             {
                 foreach (var auto in inventarioAutos)
                 {
-                    Console.WriteLine($"Marca: {auto.Marca}, Modelo: {auto.Modelo}, Stock: {auto.Stock}, Precio: {auto.Precio}");
+                    Console.WriteLine($"Marca: {auto.Marca}, Modelo: {auto.Modelo}, Placa: {auto.Placa}, Stock: {auto.Stock}, Precio: {auto.Precio}");
                 }
             }
         }
@@ -75,30 +75,81 @@ namespace CompaniaAutos
         {
             Console.WriteLine("Ingrese los detalles del vehículo:");
 
-            Console.Write("Marca: ");
-            string marca = Console.ReadLine();
-
-            Console.Write("Modelo: ");
-            string modelo = Console.ReadLine();
-
-            Console.Write("Stock: ");
-            int stock = int.Parse(Console.ReadLine());
-
-            Console.Write("Precio: ");
-            decimal precio = decimal.Parse(Console.ReadLine());
-
-            Auto nuevoAuto = new Auto
+            try
             {
-                Marca = marca,
-                Modelo = modelo,
-                Stock = stock,
-                Precio = precio
-            };
+                Console.Write("Marca: ");
+                string marca = LeerTextoAlfanumerico();
 
-            inventarioAutos.Add(nuevoAuto);
+                Console.Write("Modelo: ");
+                string modelo = LeerTextoAlfanumerico();
 
-            Console.WriteLine("El vehículo se ha agregado al inventario.");
+                Console.Write("Placa: ");
+                string placa = LeerTextoAlfanumerico();
+
+                Console.Write("Stock: ");
+                int stock = LeerNumeroEntero();
+
+                Console.Write("Precio: ");
+                decimal precio = LeerNumeroDecimal();
+
+                Auto nuevoAuto = new Auto
+                {
+                    Marca = marca,
+                    Modelo = modelo,
+                    Placa = placa,
+                    Stock = stock,
+                    Precio = precio
+                };
+
+                inventarioAutos.Add(nuevoAuto);
+
+                Console.WriteLine("El vehículo se ha agregado al inventario.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error de formato: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
+
+        static string LeerTextoAlfanumerico()
+        {
+            string input = Console.ReadLine();
+            if (!EsAlfanumerico(input))
+            {
+                throw new FormatException("Solo se permiten valores alfanuméricos.");
+            }
+            return input;
+        }
+
+        static bool EsAlfanumerico(string input)
+        {
+            return input.All(c => char.IsLetterOrDigit(c));
+        }
+
+        static int LeerNumeroEntero()
+        {
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out int numero))
+            {
+                throw new FormatException("Debe ingresar un número entero válido.");
+            }
+            return numero;
+        }
+
+        static decimal LeerNumeroDecimal()
+        {
+            string input = Console.ReadLine();
+            if (!decimal.TryParse(input, out decimal numero))
+            {
+                throw new FormatException("Debe ingresar un valor decimal válido.");
+            }
+            return numero;
+        }
+
 
         static void RealizarVenta()
         {
@@ -107,8 +158,8 @@ namespace CompaniaAutos
             Console.Write("Marca del auto: ");
             string marca = Console.ReadLine();
 
-            Console.Write("Modelo del auto: ");
-            string modelo = Console.ReadLine();
+            Console.Write("Placa del auto: ");
+            string placa = Console.ReadLine();
 
             Console.Write("DNI del cliente: ");
             string dniCliente = Console.ReadLine();
@@ -116,7 +167,7 @@ namespace CompaniaAutos
             Auto autoSeleccionado = null;
             foreach (var auto in inventarioAutos)
             {
-                if (auto.Marca == marca && auto.Modelo == modelo)
+                if (auto.Marca == marca && auto.Placa == placa)
                 {
                     autoSeleccionado = auto;
                     break;
@@ -155,6 +206,10 @@ namespace CompaniaAutos
                     ventasRealizadas.Add(nuevaVenta);
 
                     Console.WriteLine("La venta se ha realizado exitosamente.");
+
+                    Console.WriteLine($"Fecha: {nuevaVenta.FechaVenta}, Marca: {nuevaVenta.MarcaAuto}, Modelo: {nuevaVenta.ModeloAuto}, Cantidad: {nuevaVenta.Cantidad}, Monto total: {nuevaVenta.MontoTotal}");
+                    
+
                 }
                 else
                 {
@@ -171,9 +226,16 @@ namespace CompaniaAutos
         {
             Console.WriteLine("Ventas realizadas:");
 
-            foreach (var venta in ventasRealizadas)
+            if (ventasRealizadas.Count == 0)
             {
-                Console.WriteLine($"Fecha: {venta.FechaVenta}, Marca: {venta.MarcaAuto}, Modelo: {venta.ModeloAuto}, Cantidad: {venta.Cantidad}, Monto total: {venta.MontoTotal}");
+                Console.WriteLine("No se ha realizado ninguna venta.");
+            }
+            else
+            {
+                foreach (var venta in ventasRealizadas)
+                {
+                    Console.WriteLine($"Fecha: {venta.FechaVenta}, Marca: {venta.MarcaAuto}, Modelo: {venta.ModeloAuto}, Cantidad: {venta.Cantidad}, Monto total: {venta.MontoTotal}");
+                }
             }
         }
 
@@ -181,29 +243,61 @@ namespace CompaniaAutos
         {
             Console.WriteLine("Ingrese los detalles del cliente:");
 
-            Console.Write("DNI: ");
-            string dni = Console.ReadLine();
-
-            Console.Write("Nombres: ");
-            string nombres = Console.ReadLine();
-
-            Console.Write("Edad: ");
-            int edad = int.Parse(Console.ReadLine());
-
-            Console.Write("Forma de pago: ");
-            string formaPago = Console.ReadLine();
-
-            Cliente nuevoCliente = new Cliente
+            try
             {
-                DNI = dni,
-                Nombres = nombres,
-                Edad = edad,
-                FormaPago = formaPago
-            };
+                Console.Write("DNI: ");
+                string dni = Console.ReadLine();
 
-            listaClientes.Add(nuevoCliente);
+                if (!EsNumero(dni))
+                {
+                    throw new FormatException("El DNI debe contener solo números.");
+                }
 
-            Console.WriteLine("El cliente se ha registrado exitosamente.");
+                Console.Write("Nombres: ");
+                string nombres = Console.ReadLine();
+
+                if (!EsLetra(nombres))
+                {
+                    throw new FormatException("Los nombres deben contener solo letras.");
+                }
+
+                Console.Write("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+
+                Console.Write("Forma de pago: ");
+                string formaPago = Console.ReadLine();
+
+                Cliente nuevoCliente = new Cliente
+                {
+                    DNI = dni,
+                    Nombres = nombres,
+                    Edad = edad,
+                    FormaPago = formaPago
+                };
+
+                listaClientes.Add(nuevoCliente);
+
+                Console.WriteLine("El cliente se ha registrado exitosamente.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error de formato: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
+
+        static bool EsNumero(string input)
+        {
+            return int.TryParse(input, out _);
+        }
+
+        static bool EsLetra(string input)
+        {
+            return input.All(c => char.IsLetter(c));
+        }
+
     }
 }
